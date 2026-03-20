9f5bd3aec11a834f4ca2e1cb864d4b4c02894901
dima <balecored@gmail.com>
2026-03-20 22:22:34 +0500

    ОПРЕДЕЛЕНИЯ ПЕРЕМЕННЫХ ШАПОВАЛОВ

--- app/src/main/java/com/example/pr11_03/MainActivity.java
+++ app/src/main/java/com/example/pr11_03/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.pr11_03;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -10,9 +15,43 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    LocationManager _LocationManager;
+    int ACCESS_FINE_LOCATION;
+    int ACCESS_COARSE_LOCATION;
+    TextView result;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        result = findViewById(R.id.result);
+        _LocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
     }
+
+
+    LocationListener _LocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+            if(location == null) return;
+            else {
+                String message = "";
+                if(location.getProvider().equals(LocationManager.GPS_PROVIDER)){
+                    message += "\nМестоположение определено с помощью GPS: долгота - " +
+                            location.getLatitude() + " широта - " + location.getLongitude();
+                }
+
+                if(location.getProvider().equals(LocationManager.NETWORK_PROVIDER)){
+                    message += "\nМестоположение определено с помощью интернета: догота - " +
+                            location.getLatitude() + " широта - " + location.getLongitude();
+                }
+
+                result.setText(message);
+
+            }
+        }
+    };
+
+    public
 }
